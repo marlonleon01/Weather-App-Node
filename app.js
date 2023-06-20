12d2d7cddd8853c8868688e22a1cde60ca98d4ef
Marlon Leon <ghostveene@gmail.com>
2023-06-20 11:15:16 -0400

    setup to received locations from console

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 import forecast from "./utils/forecast.js"
 
-forecast("Miami", "fl", "usa", "ENTER API KEY")
+forecast(process.argv[2], process.argv[3], process.argv[4], "Enter API KEY")
+
+console.log(process.argv[2])
