884743ad62a29d5fa31669e3f58600388cc1b071
Marlon Leon <ghostveene@gmail.com>
2023-06-20 10:15:20 -0400

    calling forecast function from forecast.js

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-import axios from "axios"
+import forecast from "./utils/forecast.js"
 
-const apiKey = "USE YOUR API KEY"
-const url = `https://api.openweathermap.org/data/2.5/weather?q=Miami,fl,usa&units=imperial&appid=${apiKey}`
-
-axios.get(url)
-    .then(response => {
-        console.log(response.data)
-    })
-    .catch(error => {
-        if (error.response && error.response.data) {
-            console.log(error.response.data.message)
-        } else {
-            console.log("Unable to connect to the internet")
-        }
-    })
+forecast("Miami", "fl", "usa", "ENTER API KEY")
