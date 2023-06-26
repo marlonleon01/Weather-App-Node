3344e68f67b41d0d820614896a39b44869c3fdf2
Marlon Leon <ghostveene@gmail.com>
2023-06-26 14:53:57 -0400

    changed string for api key to just a paramater

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import forecast from "./utils/forecast.js"
 
 
-forecast(city, "Enter API KEY", (error, data) => {
+forecast(city, apiKey, (error, data) => {
     if (error) {
         return console.log(error)
     }
