a3c186b4ffb225740b267c230405be1a00fef454
Marlon Leon <ghostveene@gmail.com>
2023-06-27 17:11:54 -0400

    added search functionality for weather

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,19 @@
-console.log("Client side javascript file is loaded")
+const weatherForm = document.getElementById("weather-form")
+const locationInput = document.getElementById("location-input")
+
+async function fetchWeather(url) {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const location = locationInput.value
+
+    fetchWeather(`http://localhost:3000/weather?address=${location}`)
+})
