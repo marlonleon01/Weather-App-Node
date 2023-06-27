2fda040ebe3e783cdb13b06cdecaf6ca7ed905ee
Marlon Leon <marlon.leon2025@gmail.com>
2023-06-27 19:30:27 -0400

    changed back the issue is with render not the application

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import forecast from "../utils/forecast.js";
 import "dotenv/config"
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 //Define paths for Express config
 const __filename = fileURLToPath(import.meta.url)
